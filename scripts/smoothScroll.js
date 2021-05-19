6b475ac0b10beac5b8b5c82a04e5d17b0b393582
Ed Chekanov <kide.da@gmail.com>
2021-05-19 11:46:01 +0300

    2nd lesson

---
+++ scripts/smoothScroll.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let scrollLinks = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
+    scrollLinks.forEach((link) => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const id = link.getAttribute('href').substring(1);
+            document.getElementById(id).scrollIntoView({
+                behavior: 'smooth'
+            });
+        })
+    });
+});
